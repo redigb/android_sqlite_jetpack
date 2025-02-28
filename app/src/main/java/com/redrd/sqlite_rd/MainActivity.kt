9d3cebf6fb24a901b156f2defaa05c270ef2774b
redigb <redrojofuego77foreve@gmail.com>
2025-02-28 07:55:54 -0500

    init data ui

---
+++ app/src/main/java/com/redrd/sqlite_rd/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.redrd.sqlite_rd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.redrd.sqlite_rd.ui.theme.Android_sqlite_rdTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android_sqlite_rdTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_sqlite_rdTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/redrd/sqlite_rd/layout/InputText.kt
@@ -0,0 +1,8 @@
+package com.redrd.sqlite_rd.layout
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InputText(){
+
+}
